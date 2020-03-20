cc145e13afee7cd8bbd2cb956f110d1f2afae715
Soraya Valle <svalleortega@gmail.com>
2020-03-20 10:36:30 +0100

    Work on favourites function and layout

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,9 @@
 'use strict';
 
-console.log('>> Ready :)');
-
 let inputSearch = document.querySelector('.search__movie');
 const btnSearch = document.querySelector('.btn__search');
 const urlBase = 'http://api.tvmaze.com/search/shows?q=';
-const showList = document.querySelector('.show__shows');
+let showList = document.querySelector('.show__shows');
 const showFavList = document.querySelector('.section__fav--movies');
 
 let shows = null;
@@ -23,34 +21,58 @@ function connectToApi() {
 
 function renderShows(arr) {
     for (let item of arr) {
+        
     let showImage = item.show.image;
 
         if (showImage !== null) {
             showList.innerHTML += `<li id=${item.show.id} class="show__list--item">
             <h3 class="show__title">${item.show.name}</h3>
             <img src="${item.show.image.medium}" alt="${item.show.name}">
+            <div class="show__item--info">
             <span>Género: ${item.show.genres}</span>
             <a class="show__link" href=${item.show.url} title="Ver ficha" target="_blank"><i class="fas fa-chevron-circle-right"></i> Ver ficha</a>
+            </div>
             </li>`;
+            
         } else { 
             showList.innerHTML += `<li id=${item.show.id} class="show__list--item">
             <h3 class="show__title">${item.show.name}</h3>
             <img src="https://via.placeholder.com/210x295/cc8383/000" alt="${item.show.name}">
+            <div class="show__item--info">
             <span>Género: ${item.show.genres}</span>
             <a class="show__link" href=${item.show.url} title="Ver ficha" target="_blank"><i class="fas fa-chevron-circle-right"></i> Ver ficha</a>
+            </div>
             </li>`;
+            
         }
+        addToFavListeners();
+    }
+    
+}
+
+
+function saveAsFav(event) {
+    const index = event.currentTarget.id;
+    if (favShows.indexOf(index) === -1) {
+        favShows.push(index);
+        alert('guardada');
     }
 }
 
-// function addToFavListeners() {
-//     showList = document.querySelectorAll('.show__list--item');
-//     for (let element of showList) {
-//         element.addEventListener('click', saveAsFav);
-//     }
-// }
+function addToFavListeners() {
+    const showListEl = document.querySelectorAll('.show__list--item');
+    for (let element of showListEl) {
+        element.addEventListener('click', saveAsFav);
+    }
+}
 
-
+function getShow(id) {
+    for (let show of shows) {
+        if (show.show.id === id) {
+            return show;
+        }
+    }
+}
 
 // Function to connect 'Enter' key to search button. 
 function inputEnter(event) {
